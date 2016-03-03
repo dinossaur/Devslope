379ac718d0623eefbe50bf5be46e7698835054ad
( ◡‿◡ ♡) <elasmosaurus@Dinossaur.local>
2016-03-02 21:16:50 -0300

    devslope folder added

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,53 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var isMyHouseOnFire: Bool = false
+
+var anotherBool = true
+
+
+if isMyHouseOnFire {
+    print("Someone get me some water!")
+} else {
+    print("Someone get some fire for my house!")
+}
+
+var result = true == true
+result = true == false
+result = false == false
+
+var accountTotal = 300.33
+var newCallOfDutyGame = 59.99
+
+if accountTotal >= newCallOfDutyGame {
+    print("I just purchased the game!")
+} else {
+    print("I don't have enough money")
+}
+
+var name = "Jackie"
+
+if name == "Jack" {
+    print("Ÿour name is Jack")
+} else {
+    print("Your name is not Jack")
+}
+
+var numberA = 25
+var numberB = 40
+var numberC = 45
+var numberD = 50
+
+var finalNumber = 100
+
+if numberA == finalNumber {
+    print(numberA)
+} else if numberB == finalNumber {
+    print(numberB)
+} else if numberC == finalNumber {
+    print(numberC)
+} else {
+    print("None of the values were equal to finalNumber")
+}
+
---
+++ PROJECT EULER.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+/*
+func euler1() -> NSInteger {
+    
+    var sum = 0
+    
+    for var i=1; i<1000; i++ {
+        if i % 3 == 0 || i % 5 == 0 {
+            
+            sum = i
+            
+        }
+    }
+
+    return sum
+}
+
+print("final sum: \(euler1())")
+
+//    3, 5, 6, 9, 12, 15, 18, 21, 25, 27, 30, 33, 35, 36, 42,45, 48, 50, 51, 54, 55, 57, 60, 63, 65, 66, 69, 72, 75, 78, 81, 84, 85, 87, 90, 93, 96, 99, 102, 105, 108, 111, 114, 117, 130
+
+
+
+
