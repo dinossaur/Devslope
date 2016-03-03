3d0e415d238424749a1826deee0ef2c5d70068a2
( ◡‿◡ ♡) <elasmosaurus@Dinossaur.local>
2016-03-02 21:41:20 -0300

    Learned Conditionals part II

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -51,3 +51,34 @@ if numberA == finalNumber {
     print("None of the values were equal to finalNumber")
 }
 
+
+//CONDITIONALS 2
+
+var myAccount = 1000.00
+var myFriendsAccount = 2000.0
+var myFriendsFriendsAccount = 3000.0
+
+if myAccount > 900 && myFriendsAccount > 1500 && myFriendsFriendsAccount > 2000 {
+    print("We've got moocho moola!")
+}
+
+var playerAAlive = true
+var playerBAlive = false
+var playerCAlive = true
+
+if playerAAlive == false || playerBAlive == false || playerCAlive == false {
+    print("One of your team members is down!")
+}
+
+
+// Can I retire?????
+var age = 23
+var account = 50
+var richUncleThatDiedWhoLeftMeAnInheritance = true
+
+if account > 70000 && age >= 60 || richUncleThatDiedWhoLeftMeAnInheritance == true {
+    print("You can retire!!!")
+} else {
+    print("nope.")
+}
+
